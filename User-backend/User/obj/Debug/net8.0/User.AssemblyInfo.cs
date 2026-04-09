141b4d1c27035cfc59b96ebaca39a6d36f9632ff
DhrutiYadav <dhrutiyadav13@gmail.com>
2026-04-09 15:43:33 +0530

    Remove default WeatherForecast sample files

--- User-backend/User/obj/Debug/net8.0/User.AssemblyInfo.cs
+++ User-backend/User/obj/Debug/net8.0/User.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("User")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b754a613f6e8d09f372c233494e29514eb493e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("User")]
 [assembly: System.Reflection.AssemblyTitleAttribute("User")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
